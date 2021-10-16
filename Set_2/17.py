8833b11ba3a55be2f51fdbf81675d961c1ed4b14
Katarzyna Mialkowska <catherinemialkowska@gmail.com>
2021-10-16 18:30:12 +0200

    .

--- Set_2/17.py
+++ Set_2/17.py
@@ -7,12 +7,12 @@ Author:
 """
 
 def sort_alf(line: str) -> str:
-    return ' '.join(sorted(list(line.split())))
+    return ' '.join(sorted(list(line.split()), key = lambda c: c.lower()) )
 
 def sort_len(line: str) -> str:
     return ' '.join(sorted(list(line.split()), key = len ))
 
-line = "bsas deeaas sx aaa"
+line = "bsas deeaas aa sx Aaa aaa Aaa "
 
 print("sort alphabetically: {}".format(sort_alf(line)))
 print("sort by length : {}".format(sort_len(line)))
