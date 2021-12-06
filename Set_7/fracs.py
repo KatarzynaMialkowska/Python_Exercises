7f79d5644c7ffd544f18f0ab3a61698718aac54c
Katarzyna Mialkowska <catherinemialkowska@gmail.com>
2021-12-06 23:39:44 +0100

    .

--- Set_7/fracs.py
+++ Set_7/fracs.py
@@ -1,4 +1,5 @@
 import unittest
+from math import gcd
 from decimal import *
 
 
@@ -11,8 +12,14 @@ class Frac:
             if(y == 0):
                 raise ValueError
             else:
-                self.x = x
-                self.y = y
+                if(isinstance(x, int) and isinstance(y, int)):
+                    div = gcd(x, y)
+                    self.x = int(x / div)
+                    self.y = int(y / div)
+                else:
+                    tem = x.as_integer_ratio()
+                    self.x = tem[0]
+                    self.y = tem[1]
         except ValueError:
             print("ERROR: denominator  can't be 0: {}/{}".format(x, y))
 
@@ -24,15 +31,15 @@ class Frac:
             # return "{}/{}".format(self.x, self.y)
 
     def __repr__(self):         # zwraca "Frac(x, y)"
-        x = (self.x).as_integer_ratio()
-        return "Frac{}".format(x)
+        # x = (self.x).as_integer_ratio()
+        return "Frac({},{})".format(self.x, self.y)
 
     # Py2.7 i Py3
     def __eq__(self, other):
-        return self.x/self.y == other.x/other.y
+        return self.x == other.x and self.y == other.y
 
     def __ne__(self, other):
-        return self.x/self.y != other.x/other.y
+        return self.x != other.x and self.y != other.y
 
     def __lt__(self, other):
         return self.x/self.y < other.x/other.y
@@ -49,56 +56,81 @@ class Frac:
         return self.x/self.y >= other.x/other.y
 
     def __add__(self, other):
-        return other + self.x/self.y
+        result = [int(self.x*other.y + self.y*other.x), int(self.y*other.y)]
+        div = gcd(result[0], result[1])
+        result[0] = int(result[0] / div)
+        result[1] = int(result[1] / div)
+        return Frac(result[0], result[1])
+
+    __radd__ = __add__
 
     def __radd__(self, other):
-        return self.x/self.y + other.x/other.y
+        result = [int(other*self.y + 1*self.x), int(1*self.y)]
+        div = gcd(result[0], result[1])
+        result[0] = int(result[0] / div)
+        result[1] = int(result[1] / div)
+        return Frac(result[0], result[1])
 
-    __radd__ = __add__              # int+frac
+        # int+frac
 
     def __sub__(self, other):
-        return other - self.x/self.y
-
-    def __rsub__(self, other):
-        return other.x/other.y - self.x/self.y
+        result = [int(self.x*other.y - self.y*other.x), int(self.y*other.y)]
+        div = gcd(result[0], result[1])
+        result[0] = int(result[0] / div)
+        result[1] = int(result[1] / div)
+        return Frac(result[0], result[1])
 
     __rsub__ = __sub__              # int-frac
 
-    def __mul__(self, other):   # frac1*frac2, frac*int
-        return self.x/self.y * other
+    def __rsub__(self, other):
+        result = [int(other*self.y - 1*self.x), int(1*self.y)]
+        div = gcd(result[0], result[1])
+        result[0] = int(result[0] / div)
+        result[1] = int(result[1] / div)
+        return Frac(result[0], result[1])
 
-    def __rmul__(self, other):   # frac1*frac2, frac*int
-        return self.x/self.y * other.x/other.y
+    def __mul__(self, other):   # frac1*frac2, frac*int
+        result = [int(self.x*other.x), int(self.y*other.y)]
+        div = gcd(result[0], result[1])
+        result[0] = int(result[0] / div)
+        result[1] = int(result[1] / div)
+        return Frac(result[0], result[1])
 
     __rmul__ = __mul__              # int*frac
 
-    """
-    def __div__(self, other):  # frac1/frac2, frac/int, Py2
-        return (self.x/self.y) / other
-
-    def __rdiv__(self, other):  # int/frac, Python 2
-        return self.x/self.y * other.y/other.x
-
-     __rdiv__ = __div__
-    """
+    def __rmul__(self, other):   # frac1*frac2, frac*int
+        result = [int(self.x*other), int(self.y)]
+        div = gcd(result[0], result[1])
+        result[0] = int(result[0] / div)
+        result[1] = int(result[1] / div)
+        return Frac(result[0], result[1])
 
     def __truediv__(self, other):  # frac1/frac2, frac/int, Py3
-        return other / (self.x/self.y)
-
-    def __rtruediv__(self, other):  # int/frac, Py3
-        return self.x/self.y * other.y/other.x
+        result = [int(self.x*other.y), int(self.y*other.x)]
+        div = gcd(result[0], result[1])
+        result[0] = int(result[0] / div)
+        result[1] = int(result[1] / div)
+        return Frac(result[0], result[1])
 
     __rtruediv__ = __truediv__
+
+    def __rtruediv__(self, other):  # int/frac, Py3
+        result = [int(self.x*1), int(self.y*other)]
+        div = gcd(result[0], result[1])
+        result[0] = int(result[0] / div)
+        result[1] = int(result[1] / div)
+        return Frac(result[0], result[1])
+
     # operatory jednoargumentowe
 
     def __pos__(self):  # +frac = (+1)*frac
         return self
 
     def __neg__(self):         # -frac = (-1)*frac
-        return (-1) * self.x/self.y
+        return (-1)*self
 
     def __invert__(self):      # odwrotnosc: ~frac
-        return self.y/self.x
+        return Frac(self.y, self.x)
 
     def __float__(self):       # float(frac)
         return float(self.x/self.y)
@@ -121,32 +153,32 @@ class TestTime(unittest.TestCase):
     def test_print(self):      # test str() i repr()
         self.assertEqual(str(self.f1), "1")
         self.assertEqual(str(self.f2), "0.5")
-        self.assertEqual(repr(self.f4), "Frac(1, 4)")
+        self.assertEqual(repr(self.f4), "Frac(1,4)")
 
     def test_cmp(self):
         self.assertTrue(Frac(1, 1) == Frac(1, 1))
         self.assertFalse(Frac(0.25) != Frac(0.25))
         self.assertTrue(Frac(2, 2) > Frac(1, 2))
 
-    def test_add(self):
-        self.assertEqual(2 + Frac(1, 2), 2.5)
-        self.assertEqual(Frac(0.25) + Frac(0.25), 0.5)
-        self.assertEqual(Frac(5, 5) + Frac(1, 5), 1.2)
-
     def test_sub(self):
-        self.assertEqual(3 - Frac(2, 2), 2)
-        self.assertEqual(Frac(0.25) - Frac(0.25), 0)
+        self.assertEqual(Frac(1, 2) - Frac(1, 2), Frac(0))
+        self.assertEqual(Frac(3, 2) - Frac(1, 2), Frac(1, 1))
+        self.assertEqual(1 - Frac(1, 2), Frac(1, 2))
+
+    def test_add(self):
+        self.assertEqual(2 + Frac(1, 2), Frac(5, 2))
+        self.assertEqual(Frac(0.25) + Frac(0.25), Frac(1, 2))
 
     def test_mul(self):
-        self.assertEqual(3 * Frac(2, 2), 3)
-        self.assertEqual(Frac(0.25) * Frac(0.25), 0.0625)
+        self.assertEqual(3 * Frac(2, 2), Frac(3, 1))
+        self.assertEqual(Frac(0.25) * Frac(0.25), Frac(1, 16))
 
     def test_div(self):
-        self.assertEqual(3 / Frac(2, 2), 3)
-        self.assertEqual(Frac(0.25) / Frac(0.25), 1)
+        self.assertEqual(3 / Frac(2, 2), Frac(1, 3))
+        self.assertEqual(Frac(0.25) / Frac(0.25), Frac(1, 1))
 
     def test_invert(self):
-        self.assertEqual(~Frac(2), 0.5)
+        self.assertEqual(~Frac(2), Frac(1, 2))
 
 
 if __name__ == "__main__":
